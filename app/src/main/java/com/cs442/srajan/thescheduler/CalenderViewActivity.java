62d696c86906c42124d82fb0f13aa55f9ef1e6a2
project <timetoprogram@gmail.com>
2017-11-08 19:39:44 -0600

    Created an activity for calendar view

---
+++ app/src/main/java/com/cs442/srajan/thescheduler/CalenderViewActivity.java
@@ -0,0 +1,13 @@
+package com.cs442.srajan.thescheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalenderViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender_view);
+    }
+}
