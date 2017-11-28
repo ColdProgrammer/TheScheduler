1d620b820c4b815a8b8654b53cddf7ede895b34e
mvarhola <m@hawk.iit.edu>
2017-11-28 17:46:04 -0600

    Add event to calendar starter code

--- app/src/main/java/com/cs442/srajan/thescheduler/MainActivity.java
+++ app/src/main/java/com/cs442/srajan/thescheduler/MainActivity.java
@@ -1,10 +1,21 @@
 package com.cs442.srajan.thescheduler;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
+import android.provider.CalendarContract;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +27,12 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
     }
 
-    public void startClassListActivity(View view){
-        Intent intent = new Intent(this,ClassListActivity.class);
+    public void startClassListActivity(View view) {
+        Intent intent = new Intent(this, ClassListActivity.class);
         startActivity(intent);
     }
 
+    public void addEventToCalendar(){
+        //TODO
+    }
 }
